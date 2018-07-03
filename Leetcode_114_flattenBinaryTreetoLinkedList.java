f536ae18d06ba55ffb2e4ad9381b7d324e36400d
mengzhuoyou <mengzhuyou@gmail.com>
2018-07-03 10:18:26 +0800

    Postorder

---
+++ Leetcode_114_flattenBinaryTreetoLinkedList.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Leetcode_114_flattenBinaryTreetoLinkedList {
+    /**
+     * Definition for a binary tree node.
+     * */
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode(int x) { val = x; }
+      }
+
+    class Solution {
+        TreeNode prev = null;
+        public void flatten(TreeNode root) {
+            if(root == null) return;
+
+            flatten(root.right);
+            flatten(root.left);
+
+            root.right = prev;
+            root.left = null;
+            prev = root;
+        }
+    }
+}
